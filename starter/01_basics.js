09ff97760d3ad635d2758b425e8f6ae97bf8067a
Laurens <Laurens-Noordover@live.nmit.ac.nz>
2025-04-07 10:06:19 +1200

    Starting Basics 01

--- starter/01_basics.js
+++ starter/01_basics.js
@@ -1,8 +1,15 @@
+
 // TODO: Create a student object with 'name' and 'age' properties
 // TODO: Write a function 'printStudentName' that logs the student's name
 
 function printStudentName() {
   // Your code here
-}
+  let student = {
+    name : "Jimmy",
+    age : 19,
+  };
+  console.log(student);
+};
+
 
 module.exports = { printStudentName };
