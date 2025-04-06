4147ce1e1bfa3782df9a8b9bca102018b5602e55
Laurens <Laurens-Noordover@live.nmit.ac.nz>
2025-04-07 11:25:51 +1200

    Finished 02

--- starter/02_parameters.js
+++ starter/02_parameters.js
@@ -1,8 +1,17 @@
 // TODO: Write a function 'totalScores' that takes an array of numbers
 // TODO: Return the sum of all numbers in the array
+const scores = [75, 80, 95];
 
 function totalScores(scores) {
   // Your code here
-}
+    
+    let total = 0;
+    for (let i=0; i <scores.length; i++){
+      total += scores[i];
+    };
+    return total;
+};
 
-module.exports = { totalScores };
+console.log(totalScores(scores));
+
+//module.exports = { totalScores };
